6f84a5284b22ba47c9d966f121ca91f8a721b5ee
alfisalvacion <jmsalvacion1@up.edu.ph>
2017-05-27 20:20:25 +0800

    changed route names for registration

--- registration/urls.py
+++ registration/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^sign-in/$', log_in, name='log_in'),
-  url(r'^sign-up/$', sign_up, name='sign_up'),
-  url(r'^log-out/$', log_out, name='log_out'),
-  url(r'^edit-profile/$', edit_profile, name='edit_profile')
+	url(r'^log-in/$', log_in, name='log_in'),
+	url(r'^sign-up/$', sign_up, name='sign_up'),
+	url(r'^log-out/$', log_out, name='log_out'),
+	url(r'^edit-profile/$', edit_profile, name='edit_profile')
 ]
