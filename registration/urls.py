8b4063285d8fa61e9f0a11e177b862dec39c7307
Generil de los Santos <generild@gmail.com>
2017-05-27 20:19:47 +0800

    errors

--- registration/urls.py
+++ registration/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 
-from . import views
+from registration import views
 
 urlpatterns = [
-  url(r'^sign-in/$', log_in, name='log_in'),
-  url(r'^sign-up/$', sign_up, name='sign_up'),
-  url(r'^log-out/$', log_out, name='log_out'),
-  url(r'^edit-profile/$', edit_profile, name='edit_profile')
+    url(r'^login/$', views.log_in, name='log_in'),
+    url(r'^signup/$', views.sign_up, name='sign_up'),
+    url(r'^logout/$', views.log_out, name='log_out'),
+    url(r'^edit-profile/$', views.edit_profile, name='edit_profile')
 ]
--- registration/views.py
+++ registration/views.py
@@ -9,105 +9,105 @@ from django.core.files.storage import FileSystemStorage
 from hackathon import views
 
 def log_in(request):
-	if request.user.is_authenticated:	
-		return redirect('')
+    if request.user.is_authenticated:	
+        return redirect('')
 
-	context = {}
+    context = {}
 
-	if request.method == 'POST':
-		username = request.POST['username']
-		password = request.POST['password']
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-		user = authenticate(username=username, password=password)
+        user = authenticate(username=username, password=password)
 
-		if user is not None:
-			login(request, user)
+        if user is not None:
+            login(request, user)
 
-			return redirect('home')
+            return redirect('home')
 
-		else:
-			context['error_message'] = 'Invalid Username or Password'
-			context['username'] = username
+        else:
+            context['error_message'] = 'Invalid Username or Password'
+            context['username'] = username
 
-	return render(request, 'login.html', context=context)
+    return render(request, 'login.html', context=context)
 
 
 def sign_up(request):
-	if request.user.is_authenticated:
-		return redirect('')
+    if request.user.is_authenticated:
+        return redirect('')
 
-	context = {}
-	data = {}
+    context = {}
+    data = {}
 
-	if request.method == 'POST':
-		data['first_name'] = request.POST.get('first_name')
-		data['last_name'] = request.POST.get('last_name')
-		data['username'] = request.POST.get('username')
-		data['email'] = request.POST.get('email')
-		data['password1'] = request.POST.get('password1')
-		data['password2'] = request.POST.get('password2')
+    if request.method == 'POST':
+        data['first_name'] = request.POST.get('first_name')
+        data['last_name'] = request.POST.get('last_name')
+        data['username'] = request.POST.get('username')
+        data['email'] = request.POST.get('email')
+        data['password1'] = request.POST.get('password1')
+        data['password2'] = request.POST.get('password2')
 
-		dataCheck = user_integrityCheck(data)
+        dataCheck = user_integrityCheck(data)
 
-		if dataCheck == True:
-			User.objects.get_or_create(first_name = data['first_name'],
-				last_name = data['last_name'], username = data['username'], password = data['password1'],
-				email = data['email'])
-			
-			user_auth = authenticate(username=data['username'], password=['password1'])
-			login(request, user_auth)
-			
-			return redirect('questions')
+        if dataCheck == True:
+            User.objects.get_or_create(first_name = data['first_name'],
+                last_name = data['last_name'], username = data['username'], password = data['password1'],
+                email = data['email'])
+            
+            user_auth = authenticate(username=data['username'], password=['password1'])
+            login(request, user_auth)
+            
+            return redirect('questions')
 
-		else:
-			context = dataCheck[1]
+        else:
+            context = dataCheck[1]
 
-	return render(request, 'home.html', context=context)
+    return render(request, 'home.html', context=context)
 
 
 def user_integrityCheck(data):
-	errors = {}
+    errors = {}
 
-	if len(User.objects.filter(username = data.get('username'))) > 0:
-		errors['username_error'] = "An account has this Username already. Pick another one."
+    if len(User.objects.filter(username = data.get('username'))) > 0:
+        errors['username_error'] = "An account has this Username already. Pick another one."
 
-		return False, errors
+        return False, errors
 
-	if len(User.objects.filter(email = data.get('email'))) > 0:
-		errors['email_error'] = "An account has this Email already. Pick another one."
+    if len(User.objects.filter(email = data.get('email'))) > 0:
+        errors['email_error'] = "An account has this Email already. Pick another one."
 
-		return False, errors
+        return False, errors
 
-	if data.get('password1') != data.get('password2'):
-		errors['password_error'] = "Passwords do not match"
+    if data.get('password1') != data.get('password2'):
+        errors['password_error'] = "Passwords do not match"
 
-	return True
+    return True
 
 
 def edit_profile(request):
-	if not request.user.is_authenticated:
-		return redirect('sign_in')
+    if not request.user.is_authenticated:
+        return redirect('sign_in')
 
-	if request.method == 'POST':
-		new_first_name = request.POST.get(new_first_name)
-		new_last_name = request.POST.get(new_first_name)
+    if request.method == 'POST':
+        new_first_name = request.POST.get(new_first_name)
+        new_last_name = request.POST.get(new_first_name)
 
-		user = request.user
+        user = request.user
 
-		user.first_name = new_first_name
-		user.last_name = new_last_name
-		user.save()
+        user.first_name = new_first_name
+        user.last_name = new_last_name
+        user.save()
 
-		user_avatar = request.FILES['avatar']
-		fs = FileSystemStorage()
-		user_avatar.name = 'user_' + request.user + '_' + user_avatar.name
-		filename = fs.save(user_avatar.name, user_avatar)
+        user_avatar = request.FILES['avatar']
+        fs = FileSystemStorage()
+        user_avatar.name = 'user_' + request.user + '_' + user_avatar.name
+        filename = fs.save(user_avatar.name, user_avatar)
 
-		person = Person.objects.get(id = request.user.id)
-		person.avatar = filename
-		person.save()
+        person = Person.objects.get(id = request.user.id)
+        person.avatar = filename
+        person.save()
 
-	return redirect('')
+    return redirect('')
 
 
 def log_out(request):
