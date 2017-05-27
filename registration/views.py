7d61f5b3c0aa9e670657073797edc478af5abf91
alfisalvacion <jmsalvacion1@up.edu.ph>
2017-05-28 00:58:58 +0800

    fixed signup redirect error

--- registration/views.py
+++ registration/views.py
@@ -62,7 +62,7 @@ def sign_up(request):
         else:
             context = dataCheck[1]
 
-    return render(request, 'edit_profile.html', context=context)
+    return render(request, 'register.html', context=context)
 
 
 def user_integrityCheck(data):
