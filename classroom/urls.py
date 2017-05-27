816f66813bcd214cd96d957803d98e9ab3c4ba28
riidatibula <riidatibula@gmail.com>
2017-05-27 22:54:31 +0800

    edited

--- classroom/urls.py
+++ classroom/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 	url(r'^(?P<pk>\d+)/forum/$', view.forum, name='forum'),
 	url(r'^(?P<pk>\d+)/topic/$', view.topic, name='topic'),
 	url(r'^(?P<pk>\d+)/topic/(?P<topic_id>\d+)/$', view.topic_details, name='topic_details'),
+	url(r'^(?P<pk>\d+)/topic/(?P<topic_id>\d+)/add-post/$', view.add_topic_post, name='add_topic_post'),
 	url(r'^(?P<pk>\d+)/topic/add-topic/$', view.add_topic, name='add_topic'),	
 ]
