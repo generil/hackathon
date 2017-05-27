c4e145dbcc01804832ba6d53893a4de0c94b3cbb
Generil de los Santos <generild@gmail.com>
2017-05-27 21:43:51 +0800

    naega hajima hajima

---
+++ hackathon/static/js/app.js
@@ -0,0 +1,24 @@
+$(document).on('change', ':file', function() {
+  var input = $(this),
+    numFiles = input.get(0).files ? input.get(0).files.length : 1,
+    label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+  input.trigger('fileselect', [numFiles, label]);
+
+  document.getElementById('add-t-image').innerText = 'Change image';
+  var img = document.getElementsByClassName('imagepreview');
+  img[0].style.display = 'block';
+});
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      $('#preview').attr('src', e.target.result);
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$('input[type="file"]').change(function() {
+  readURL(this);
+});
